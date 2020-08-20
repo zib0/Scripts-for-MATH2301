777bc049f680617ec78eff95d32dffc499b24b70
Zibo Wang <zibo.w@outlook.com>
2020-08-20 11:52:52 +1000

    add linter and formatter

--- count_paths.py
+++ count_paths.py
@@ -13,6 +13,8 @@ import numpy as np
 @param t: the vertex to
 @return the number of paths of length l from f to t.
 '''
+
+
 def count_paths(m, l, f, t):
     result = np.linalg.matrix_power(m, l)
     return result[f-1][t-1]
@@ -20,9 +22,9 @@ def count_paths(m, l, f, t):
 
 if __name__ == "__main__":
     # Please edit your adjacency matrix here.
-    adj_mtx = ([0,1,0,0],
-               [0,0,1,0],
-               [0,0,0,1],
-               [0,0,1,0])
+    adj_mtx = ([0, 1, 0, 0],
+               [0, 0, 1, 0],
+               [0, 0, 0, 1],
+               [0, 0, 1, 0])
 
     print(count_paths(adj_mtx, 2, 1, 3))
